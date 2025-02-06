5e9389a89ac6e4ae6e033a3bfc0ef6963d80e6f7
flotr18 <trouilletflorent@outlook.com>
2025-02-06 14:47:34 +0100

    Fix Code Error

---
+++ BuzzFizz.py
@@ -0,0 +1,13 @@
+def buzzfizz():
+    for i in range(101):
+        if i % 3 == 0 and i % 5 == 0:
+            i = "FizzBuzz"
+        elif i % 5 == 0:
+            i = "Buzz"
+        elif i % 3 == 0:
+            i = "Fizz"
+        print(i)
+
+            
+
+buzzfizz()
